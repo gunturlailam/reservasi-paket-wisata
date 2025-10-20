e7db1a50fe4f1edc0ab0c61bd4be4e211827eaf9
Guntur Lailam Yuro <gunturlailamyuro8@gmail.com>
2025-10-21 00:22:52 +0700

    fitur: tambahkan migration untuk tabel pembayaran detail

---
+++ app/Database/Migrations/2025-10-20-172023_PemesananDetail.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class PemesananDetail extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            'id' => [
+                'type' => 'INT',
+                'constraint' => 11,
+                'unsigned' => true,
+                'auto_increment' => true,
+            ],
+            'id_pemesanan' => [
+                'type' => 'INT',
+                'constraint' => 11,
+                'unsigned' => true,
+                'null' => true,
+            ],
+            'tanggal_berangkat' => [
+                'type' => 'DATE',
+                'null' => true,
+            ],
+            'tanggal_kembali' => [
+                'type' => 'DATE',
+                'null' => true,
+            ],
+            'jumlah_penumpang' => [
+                'type' => 'DOUBLE',
+                'null' => true,
+            ]
+        ]);
+
+        // Tambahkan primary key
+        $this->forge->addKey('id', true);
+
+
+        // Tambahkan foreign key
+        $this->forge->addForeignKey('id_pemesanan', 'pemesanan', 'id', 'SET NULL', 'CASCADE');
+
+        // Buat tabel
+        $this->forge->createTable('pembayaran_detail', true);
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('pembayaran_detail', true);
+    }
+}
