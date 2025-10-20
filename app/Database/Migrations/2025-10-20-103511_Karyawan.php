7e7a00c4a4f8d262c59a61adfcad2e216a5e00fd
Guntur Lailam Yuro <gunturlailamyuro8@gmail.com>
2025-10-20 17:43:31 +0700

    fitur: tambahkan migration untuk tabel karyawab

---
+++ app/Database/Migrations/2025-10-20-103511_Karyawan.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class Karyawan extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            'idkaryawan' => [
+                'type' => 'INT',
+                'constraint' => 11,
+                'unsigned' => true,
+                'auto_increment' => true,
+            ],
+            'idjabatan' => [
+                'type' => 'INT',
+                'constraint' => 11,
+                'unsigned' => true,
+                'null' => true,
+            ],
+            'namakaryawan' => [
+                'type' => 'VARCHAR',
+                'constraint' => 100,
+                'null' => true,
+            ],
+            'alamat' => [
+                'type' => 'VARCHAR',
+                'constraint' => 200,
+                'null' => true,
+            ],
+            'nohp' => [
+                'type' => 'VARCHAR',
+                'constraint' => 20,
+                'null' => true,
+            ]
+        ]);
+
+        // Tambahkan primary key
+        $this->forge->addKey('idkaryawan', true);
+
+        // Tambahkan foreign key
+        $this->forge->addForeignKey('idjabatan', 'jabatan', 'id', 'SET NULL', 'CASCADE');
+
+        // Buat tabel
+        $this->forge->createTable('karyawan', true);
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('karyawan', true);
+    }
+}
